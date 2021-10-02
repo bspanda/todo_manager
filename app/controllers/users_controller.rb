c641a60e4f07afc1ebf4d56cc3d02ca618d2974c
BSPANDA <panda.bs@raghuenggcollege.in>
2021-10-02 11:35:02 +0530

    My Todo Manager app Project

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,35 @@
+class UsersController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def index
+    # render plain: "Hello, this is /todos! #{DateTime.now.to_s(:short)}"
+    render plain: User.order(:name).map { |user| user.to_display_string }.join("\n")
+  end
+
+  def show
+    id = params[:id]
+    user = User.find(id)
+    render plain: user.to_display_string
+  end
+
+  def create
+    user_name = params[:name]
+    user_email = params[:email]
+    user_pass = params[:password]
+    new_user = User.create!(
+      name: user_name,
+      email: user_email,
+      password: user_pass,
+    )
+    response_text = "hay, the new user is  : #{new_user.id}"
+    render plain: response_text
+  end
+
+  def login
+    user_email = params[:email]
+    user_pass = params[:password]
+    user = User.where("email = ? and password = ?", user_email, user_pass)
+    user_status = user.empty? ? "false" : "true"
+    render plain: "#{user_status}"
+  end
+end
